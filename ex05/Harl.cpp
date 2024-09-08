616f550c6325d7fe4c9f92f251afdb7780683226
joonwan <joonwan1024@naver.com>
2024-09-09 04:08:55 +0900

    dev :ex05

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -2,34 +2,37 @@
 
 void Harl::debug(void)
 {
-
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
 }
 
 void Harl::info(void)
 {
-
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put \
+                enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void Harl::warning(void)
 {
-
+    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for \
+                years whereas you started working here since last month."<< std::endl;
 }
 
 void Harl::error(void)
 {
-
+    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
 void Harl::complain(std::string level)
 {
-    void (*f)();
+    int i;
 
-    if (level == "DEBUG")
-        f = debug;
-    else if (level == "INFO")
-        f = info;
-    else if (level == "WARNING")
-        f = warning;
-    else if (level == "ERROR")
-        f = error;
+    i = 0;
+    std::string names[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+    void (Harl::*fp[4])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+    while (i < 4)
+    {
+        if (names[i] == level)
+            fp[i]();
+        i++;
+    }
 }
