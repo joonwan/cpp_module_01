09900319a88b45967fb6bb595bb620918a20becf
joonwan <joonwan1024@naver.com>
2024-09-09 17:17:30 +0900

    dev :ex05

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -2,23 +2,25 @@
 
 void Harl::debug(void)
 {
+    std::cout << "[DEBUG] : ";
     std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
 }
 
 void Harl::info(void)
 {
-    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put \
-                enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+    std::cout << "[INFO] : ";
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void Harl::warning(void)
 {
-    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for \
-                years whereas you started working here since last month."<< std::endl;
+    std::cout << "[WARNING] : ";
+    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month."<< std::endl;
 }
 
 void Harl::error(void)
 {
+    std::cout << "[ERROR] : ";
     std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
@@ -28,11 +30,16 @@ void Harl::complain(std::string level)
 
     i = 0;
     std::string names[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-    void (Harl::*fp[4])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+    void (Harl::*fp[4])(void);
+    fp[0] = &Harl::debug;
+    fp[1] = &Harl::info;
+    fp[2] = &Harl::warning;
+    fp[3] = &Harl::error;
+
     while (i < 4)
     {
         if (names[i] == level)
-            fp[i]();
+            (this ->*fp[i])();
         i++;
     }
 }
